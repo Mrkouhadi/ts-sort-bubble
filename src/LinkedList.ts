e740babe27f519a5243a059a256b05f64d3722bb
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-08-16 01:13:35 +0800

    added swap and  print methods

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -48,4 +48,41 @@ export class LinkedList{
         }
         throw new Error("Index is out of bounds !")
     }
+
+    compare(leftIndex:number, rightIndex:number):boolean{
+       if(!this.head){
+           throw new Error("Your List is empty")
+       }
+       return this.at(leftIndex).data > this.at(rightIndex).data;
+    }
+
+    swap(leftIndex:number, rightIndex:number):void{
+        const leftNode = this.at(leftIndex);
+        const rightNode = this.at(rightIndex);
+
+        const leftHand = leftNode.data;
+        leftNode.data = rightNode.data;
+        rightNode.data = leftHand;
+    }
+
+    print():void{
+        if(!this.head){
+            return;
+        }
+
+        let node: Node | null = this.head;
+        while(node){
+            console.log(node.data);
+            node = node.next;
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
